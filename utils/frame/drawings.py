ac74d5e2a1eb4b6b700f4910fa8a1021fd144eab
walbermr <wmr@cin.ufpe.br>
2025-09-30 19:28:51 -0300

    fix bbox vis

--- utils/frame/drawings.py
+++ utils/frame/drawings.py
@@ -9,27 +9,33 @@ def getBboxes(outputs, frame_resolution, score_thr, removable_area:List[Rectangl
     boxes:List[Rectangle] = []
 
     for output in outputs:
-        try:
-            output[0].clip(0, frame_resolution[1])  # x1
-            output[1].clip(0, frame_resolution[0])  # y1
-            output[2].clip(0, frame_resolution[1])  # x2
-            output[3].clip(0, frame_resolution[0])  # y2
+        for box in output:
+            if len(box) == 0: continue
 
-            for box in output:
-                x1, y1, x2, y2, score, index = box
-                if score > score_thr:
-                    x, y, h, w = int(x1), int(y1), int(abs(x1-x2)), int(abs(y1-y2))
-                    bbox = BoundingBox(x, y, h, w)
-                    remove_box = False
-                    if len(removable_area) > 0:
-                        for area in removable_area:
-                            remove_box |= area.contains(bbox.center)
+            box = [
+                max(box[0], frame_resolution[1]),
+                max(box[1], frame_resolution[0]),
+                max(box[2], frame_resolution[1]),
+                max(box[3], frame_resolution[0]),
+                box[4],
+                box[5],
+            ]   # transform from tupleto lix, avoiding modifying the underlying data
 
-                    if not remove_box:
-                        boxes.append(bbox)
-                        rects.append((x, y, x+w, y+h))
-        except:
-            print("No detections.")
+            x1, y1, x2, y2, score, index = box
+            if score > score_thr:
+                x, y, h, w = int(x1), int(y1), int(abs(x1-x2)), int(abs(y1-y2))
+                bbox = BoundingBox(x, y, h, w)
+                remove_box = False
+                if len(removable_area) > 0:
+                    for area in removable_area:
+                        remove_box |= area.contains(bbox.center)
+
+                if not remove_box:
+                    boxes.append(bbox)
+                    rects.append((x, y, x+w, y+h))
+    
+    if len(boxes) == 0:
+        print("No detections.")
 
     return rects, boxes
 
