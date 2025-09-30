49dfccbc3114edeb7083264c4617f6a5fe57040c
walbermr <wmr@cin.ufpe.br>
2025-09-30 19:28:35 -0300

    fix cv algo output

--- detection/traditional/cv.py
+++ detection/traditional/cv.py
@@ -5,8 +5,8 @@ class CVDetector:
     def __init__(self):
         self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
     
-    def inference(self, inputFrmae, *args, **kwargs):
-        gray = cv2.cvtColor(inputFrmae, cv2.COLOR_BGR2GRAY)
+    def inference(self, inputFrame, *args, **kwargs):
+        gray = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 100, 200)
         thresh = cv2.threshold(edges, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
@@ -21,8 +21,9 @@ class CVDetector:
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
             area = w * h
+            #TODO: PARAMETRIZE MAX AREA
             if 0 < area < 200:
-                valid.append((x, y, x+w, y+h, 1.0, i))
+                valid.append([(x, y, x+w, y+h, 1.0, i)])
                 i += 1
 
         return valid
